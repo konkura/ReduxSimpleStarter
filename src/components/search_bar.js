fd7efdb3e62ed6d86ebb41243f1dd2be70f3c9d2
Phil Worthington <phil@konkura.com>
2018-01-10 11:43:51 +0000

    Really nice simple video picker

---
+++ src/components/search_bar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+  render() {
+    return (
+      <div className="search-bar">
+        <input value={this.state.term} onChange={event => this.setState({ term: event.target.value })} />
+        State: {this.state.term}
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCCrS4DQxMGfQsxuNMcaIS3mx-VWb9msm0';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create default component to generate HTML.  
+// This is a stateless, functional component when defined like this, whereas searchbar is defined using class so it has state
+// If we want a component to have state we need to use class
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>
+//   );
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: [], selectedVideo: null };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' },  (data) => { //If this was function(data) {this.setState...} we hit problems with this not being defined!
+      this.setState({videos: data, selectedVideo: data[0]}); 
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
+
+// Render the HTML to the 'container' DOM element in index.html
+ReactDOM.render(<App />, document.querySelector('.container'));
