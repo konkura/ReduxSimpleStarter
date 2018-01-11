49111900a4778db3d0f78fd0ef840a06b5fd1053
Phil Worthington <phil@konkura.com>
2018-01-11 12:57:08 +0000

    final version

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,10 +9,15 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="search-bar">
-        <input value={this.state.term} onChange={event => this.setState({ term: event.target.value })} />
+        <input value={this.state.term} onChange={event => this.onInputChange(event.target.value)} />
         State: {this.state.term}
       </div>
     );
+  }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 const API_KEY = 'AIzaSyCCrS4DQxMGfQsxuNMcaIS3mx-VWb9msm0';
 
 
@@ -22,16 +23,21 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = { videos: [], selectedVideo: null };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({ key: API_KEY, term: 'surfboards' },  (data) => { //If this was function(data) {this.setState...} we hit problems with this not being defined!
+  videoSearch(term)
+  {
+    YTSearch({ key: API_KEY, term: term },  (data) => { //If this was function(data) {this.setState...} we hit problems with this not being defined!
       this.setState({videos: data, selectedVideo: data[0]}); 
     });
   }
   
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300); //Create throttled version of videoSearch so it only searches every 300ms not every key press
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
       </div>
